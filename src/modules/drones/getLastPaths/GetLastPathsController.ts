efd698ca0374a4914e68cdabf3feb76bdaa79610
José Rômulo <joseromulo.10@hotmail.com>
2023-05-20 12:27:19 -0300

    feat: add module to get last paths

---
+++ src/modules/drones/getLastPaths/GetLastPathsController.ts
@@ -0,0 +1,14 @@
+import {Request, Response} from 'express'
+import { GetLastPathsService } from './GetLastPathsService'
+
+class GetLastPathsController {
+    async handle(request: Request, response: Response) {
+        const getLastPathService = new GetLastPathsService()
+        
+        const lastPaths = await getLastPathService.execute()
+
+        return response.status(200).json(lastPaths)
+    }
+}
+
+export { GetLastPathsController }
---
+++ src/modules/drones/getLastPaths/GetLastPathsService.ts
@@ -0,0 +1,12 @@
+import Knex from 'knex'
+import { development } from '../../../../knexfile'
+
+class GetLastPathsService {
+    async execute(){
+        const knex = Knex(development)
+
+        return await knex('drones_path').orderBy('created_at', 'desc').limit(10).select('*')
+    }
+}
+
+export { GetLastPathsService }
